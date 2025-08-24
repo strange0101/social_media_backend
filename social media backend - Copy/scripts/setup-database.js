e0961b3ea84d9979afce2d4a70833acbb3ebce95
Aryan Sahu <121814870+strange0101@users.noreply.github.com>
2025-08-24 15:18:28 +0530

    Update setup-database.js

--- social media backend - Copy/scripts/setup-database.js
+++ social media backend - Copy/scripts/setup-database.js
@@ -73,17 +73,7 @@ const setupDatabase = async () => {
 		});
 		
 		} 
-		else {
-		// For local development
-		pool = new Pool({
-			host: process.env.DB_HOST,
-			port: process.env.DB_PORT,
-			database: process.env.DB_NAME,
-			user: process.env.DB_USER,
-			password: process.env.DB_PASSWORD,
-		});
 		
-	}
 
 	try {
 		console.log("Setting up database...");
